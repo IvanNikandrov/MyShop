56426ab9bc45c4dfdcd42233465913c191c64dfa
ivanNikandrov <ivannikandrov1@gmail.com>
2022-09-08 18:16:32 +0300

    update base.html add bootstrap template

--- store/views.py
+++ store/views.py
@@ -4,4 +4,5 @@ from .models import Category, Product
 
 def all_products(request):
     products = Product.objects.all()
-    return render(request, 'store/home.html', {'products': products})
+    categories = Category.objects.all()
+    return render(request, 'store/home.html', {'products': products, 'categories': categories})
